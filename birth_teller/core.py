ee8ef1f6b82b44e54a729643e4be4a453cb307a8
s4gor <imrans4gor@gmail.com>
2025-08-20 15:59:21 +0600

    fixed linting

--- birth_teller/core.py
+++ birth_teller/core.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date
 
 
 class BTM:
@@ -23,26 +23,26 @@ class BTM:
         print(f'\n* {name}, welcome to BTM *\n')
     
     def zodiac_sign(self, day, month):
-        
+
         zodiac_dates = [
-        ("Capricorn", (12, 22), (1, 19)),
-        ("Aquarius", (1, 20), (2, 18)),
-        ("Pisces", (2, 19), (3, 20)),
-        ("Aries", (3, 21), (4, 19)),
-        ("Taurus", (4, 20), (5, 20)),
-        ("Gemini", (5, 21), (6, 20)),
-        ("Cancer", (6, 21), (7, 22)),
-        ("Leo", (7, 23), (8, 22)),
-        ("Virgo", (8, 23), (9, 22)),
-        ("Libra", (9, 23), (10, 22)),
-        ("Scorpio", (10, 23), (11, 21)),
-        ("Sagittarius", (11, 22), (12, 21)),
+            ("Capricorn", (12, 22), (1, 19)),
+            ("Aquarius", (1, 20), (2, 18)),
+            ("Pisces", (2, 19), (3, 20)),
+            ("Aries", (3, 21), (4, 19)),
+            ("Taurus", (4, 20), (5, 20)),
+            ("Gemini", (5, 21), (6, 20)),
+            ("Cancer", (6, 21), (7, 22)),
+            ("Leo", (7, 23), (8, 22)),
+            ("Virgo", (8, 23), (9, 22)),
+            ("Libra", (9, 23), (10, 22)),
+            ("Scorpio", (10, 23), (11, 21)),
+            ("Sagittarius", (11, 22), (12, 21)),
         ]
 
         for sign, (start_month, start_day), (end_month, end_day) in zodiac_dates:
             if (month == start_month and day >= start_day) or (month == end_month and day <= end_day):
                 return sign
-        
+
         return "Invalid date"
 
     def validate_date(self, day, month, year):
